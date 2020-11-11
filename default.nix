6023aeb595a8b124d9892da7a0006a885e618da6
Ashesh Ambasta <ashesh.ambasta@gmail.com>
2020-11-11 12:02:22 +0100

    Use reflex-platform.

--- default.nix
+++ default.nix
@@ -1,1 +1,21 @@
-(import ./release.nix).prelude-polysemy
+{ system ? builtins.currentSystem }:
+let
+  sources = import ./nix/sources.nix;
+  rp = import sources.reflex-platform { inherit system; };
+in rp.project ({ pkgs, ... }: {
+
+  name = "prelude-polysemy";
+
+  # uses jsaddle-warp to fire up a warp server instead of webgtk.
+  useWarp = true;
+  withHoogle = false;
+
+  packages = {
+    prelude-polysemy = ./.;
+  };
+
+  shells = {
+    ghc = [ "prelude-polysemy" ];
+    ghcjs = [ "prelude-polysemy" ];
+  }; 
+})
--- shell.nix
+++ shell.nix
@@ -1,11 +1,1 @@
-let
-  sources = import ./nix/sources.nix;
-  nixpkgs = (import sources.nixpkgs) { };
-  release = import ./release.nix;
-in nixpkgs.haskellPackages.shellFor {
-  packages = p: builtins.attrValues release;
-  buildInputs = with nixpkgs; [
-    haskellPackages.cabal-install
-    haskellPackages.ghcid
-  ];
-}
+(import ./. {}).shells.ghc
