e6958ea044dac5b809b09176ebac3a2214991a27
Ashesh Ambasta <ashesh.ambasta@gmail.com>
2020-11-11 09:51:17 +0100

    Add a custom prelude.

--- src/Prelude.hs
+++ src/Prelude.hs
@@ -1,6 +1,24 @@
 module Prelude
   ( module Protolude
+  , module Err
+  , module Log
+  , module HTTP
+  -- * Text
+  , pack
+  , unpack
+  , pShow
   )
 where
 
-import           Protolude
+import           Data.Text.Lazy                 ( toStrict )
+import qualified Text.Pretty.Simple            as PS
+import           Network.HTTP.Types            as HTTP
+import           Data.Text                      ( pack
+                                                , unpack
+                                                )
+import           Protolude               hiding ( catch )
+import           Prelude.Control.Error         as Err
+import           Prelude.Control.Log           as Log
+
+pShow :: Show a => a -> Text
+pShow = toStrict . PS.pShow
